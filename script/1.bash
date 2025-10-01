ff689b2eb56a2df45404962032602dfd54681279
jhlee <jhlee_bio@naver.com>
2025-10-01 13:38:29 +0900

    Update 1.bash

--- script/1.bash
+++ script/1.bash
@@ -1,1 +1,26 @@
+# ---------- 1) Prepare GSE102130 ----------
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE102nnn/GSE102130/suppl/GSE102130%5FK27Mproject.RSEM.vh20170621.txt.gz
+
+
+# ---------- 2) Prepare github NetworkPractice ----------
+git clone https://github.com/neocaleb/NetworkPractice
+
+
+# ---------- 3) Prepare GSE102130 ----------
+
+
+
+# ---------- 4) Prepare GSE102130 ----------
+
+
+
+
+# ---------- 5) Prepare GSE102130 ----------
+
+
+
+# ---------- 6) Prepare GSE102130 ----------
+
+
+
+# ---------- 7) Prepare GSE102130 ----------
